3c0033a769f4e18b4cded306dcd2f9247d5df5bd
philip <philipbwalya2000@gmail.com>
2023-10-28 16:38:37 +0200

    react modern bank app

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({ styles }) => {
+  return (
+    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-[10px] ${styles}`}>Get started</button>
+  )
+}
+
+export default Button;
